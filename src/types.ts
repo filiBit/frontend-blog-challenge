4f6ac86be79caa5d31775420d19e18eb2a2093c9
Filip Biterski <filip.biterski@filibit.com>
2025-08-28 01:23:20 +0200

    feat: ts types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface BlogPost {
+    id: number;
+    title: string;
+    body: string;
+    authorName: string;
+    authorEmail: string;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+}
