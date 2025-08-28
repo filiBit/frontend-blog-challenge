53203263b5aec66e9815bbee6af71aea36746519
Filip Biterski <filip.biterski@filibit.com>
2025-08-28 02:07:42 +0200

    fix: home page error handling

--- src/app/home-page.ts
+++ src/app/home-page.ts
@@ -10,10 +10,20 @@ import { MatIconModule } from "@angular/material/icon";
 })
 export class HomePage {
     blogService = inject(BlogService);
+
     protected blogPosts: BlogPost[] = [];
+    protected isBogPostsError: boolean = false;
+
     ngOnInit() {
-        this.blogService.getRecentBlogPosts().then((blogPosts) => {
-            this.blogPosts = blogPosts;
-        });
+        this.blogService
+            .getRecentBlogPosts()
+            .then((blogPosts) => {
+                this.blogPosts = blogPosts;
+                this.isBogPostsError = false;
+            })
+            .catch(() => {
+                this.isBogPostsError = true;
+                this.blogPosts = [];
+            });
     }
 }
